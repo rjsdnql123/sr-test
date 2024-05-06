c68e793c11225a019089423e43f48e261ee8ee99
rjsdnql123 <rjsdnql123@gmail.com>
2024-05-07 01:52:13 +0900

    (Feat): Front-로그인 안되어 있으면 home으로 이동 시키도록 개발

--- frontend/src/pages/Router.tsx
+++ frontend/src/pages/Router.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import IndexPage from "./index";
+import HomePage from "./home";
+import PrivateRoute from "./PrivateRoute";
 
 const RouterIndex = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/home/*" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
