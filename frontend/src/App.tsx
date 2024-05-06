59c1a0456fefcd662039a724ebb7886c791ef53e
rjsdnql123 <rjsdnql123@gmail.com>
2024-05-07 02:15:20 +0900

    (Fix): Front-다크모드 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
 import RouterIndex from "./pages/Router";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { colors } from "@mui/material";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: colors.green[200],
+    },
+  }
+});
 
 function App() {
-  return <RouterIndex />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <RouterIndex />
+    </ThemeProvider>
+  );
 }
 
 export default App;
